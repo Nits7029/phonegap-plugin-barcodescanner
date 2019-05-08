347af27397a14fad8fbcbec8d6d9b034546370e6
Ravi Sojitra <ravisojitra79@gmail.com>
2019-05-08 17:22:07 +0530

    removing permission dependency

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -22,7 +22,6 @@ import android.content.pm.PackageManager;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.PermissionHelper;
 
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.android.encode.EncodeActivity;
@@ -115,11 +114,7 @@ public class BarcodeScanner extends CordovaPlugin {
         } else if (action.equals(SCAN)) {
 
             //android permission auto add
-            if(!hasPermisssion()) {
-              requestPermissions(0);
-            } else {
-              scan(args);
-            }
+            scan(args);
         } else {
             return false;
         }
@@ -259,59 +254,6 @@ public class BarcodeScanner extends CordovaPlugin {
 
         this.cordova.getActivity().startActivity(intentEncode);
     }
-
-    /**
-     * check application's permissions
-     */
-   public boolean hasPermisssion() {
-       for(String p : permissions)
-       {
-           if(!PermissionHelper.hasPermission(this, p))
-           {
-               return false;
-           }
-       }
-       return true;
-   }
-
-    /**
-     * We override this so that we can access the permissions variable, which no longer exists in
-     * the parent class, since we can't initialize it reliably in the constructor!
-     *
-     * @param requestCode The code to get request action
-     */
-   public void requestPermissions(int requestCode)
-   {
-       PermissionHelper.requestPermissions(this, requestCode, permissions);
-   }
-
-   /**
-   * processes the result of permission request
-   *
-   * @param requestCode The code to get request action
-   * @param permissions The collection of permissions
-   * @param grantResults The result of grant
-   */
-  public void onRequestPermissionResult(int requestCode, String[] permissions,
-                                         int[] grantResults) throws JSONException
-   {
-       PluginResult result;
-       for (int r : grantResults) {
-           if (r == PackageManager.PERMISSION_DENIED) {
-               Log.d(LOG_TAG, "Permission Denied!");
-               result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
-               this.callbackContext.sendPluginResult(result);
-               return;
-           }
-       }
-
-       switch(requestCode)
-       {
-           case 0:
-               scan(this.requestArgs);
-               break;
-       }
-   }
 
     /**
      * This plugin launches an external Activity when the camera is opened, so we
